367e5f89b6c2e217b1a3525d48bd5fd233bce6d7
PuchNik <i.p.chn.k@gmail.com>
2023-10-26 19:16:16 +0300

    Change name list_and_operations

--- Списки и операции/Списки и операции.py
+++ Списки и операции/Списки и операции.py
@@ -1,57 +1,54 @@
-cities = ['Moscow', 'LA', 'CH']
-print(cities)
-
-
-full = ['hi', 7, True, 12.3, False, [1, 2, 3], -7]
-print(full)
-
-full[3] = 3
-
-print(full)
-
-b = list()
-print(b)
-
-b = list([True, False])
-print(b)
-
-b = list("Python")
-print(b)
-
-t = [23.5, 25.6, 27.3, 26.0, 30.4, 29.5]
-
-t_sort = sorted(t)
-print(t_sort)
-
-t_sort = sorted(t, reverse=True)
-print(t_sort)
-
-a = [1, 2]
-del a[0]
-b = a + [True]
-print(b)
-
-c = b * 3
-print(c)
-
-print(1 in a)
-print(3 in a)
-
-del a[0]
-print(b)
-
-
-lst = input()
-print(lst)
-
-lst2 = lst.split()
-print(lst2)
-
-lst = input().split()
-print(lst)
-
-
-
+# cities = ['Moscow', 'LA', 'CH']
+# print(cities)
+#
+#
+# full = ['hi', 7, True, 12.3, False, [1, 2, 3], -7]
+# print(full)
+#
+# full[3] = 3
+#
+# print(full)
+#
+# b = list()
+# print(b)
+#
+# b = list([True, False])
+# print(b)
+#
+# b = list("Python")
+# print(b)
+#
+# t = [23.5, 25.6, 27.3, 26.0, 30.4, 29.5]
+#
+# t_sort = sorted(t)
+# print(t_sort)
+#
+# t_sort = sorted(t, reverse=True)
+# print(t_sort)
+#
+# a = [1, 2]
+# del a[0]
+# b = a + [True]
+# print(b)
+#
+# c = b * 3
+# print(c)
+#
+# print(1 in a)
+# print(3 in a)
+#
+# del a[0]
+# print(b)
+#
+#
+# lst = input()
+# print(lst)
+#
+# lst2 = lst.split()
+# print(lst2)
+#
+# lst = input().split()
+# print(lst)
 
 
 
