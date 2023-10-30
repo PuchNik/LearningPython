884244dce91f0ef729fe4d0297ece3d861fa2d92
PuchNik <i.p.chn.k@gmail.com>
2023-10-30 15:07:13 +0300

    Add new lesson

---
+++ lst_methods/lst_methods.py
@@ -0,0 +1,34 @@
+marks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
+
+# marks.remove(marks[2])
+# print(marks)
+
+# marks.remove(True)
+# print(marks)
+
+# marks.append(0)
+# print(marks)
+
+# marks.remove(False)
+# print(marks)
+#
+# a = marks.copy()
+# a.append(10)
+#
+# print(marks)
+# print(a)
+
+# print(f"id: {id(marks)} != id: {id(a)}")
+
+# print(marks.count(1))
+# print(marks.count(10))
+
+# print(marks.index(1))
+# print(marks.index(1, 2))
+#
+# marks.reverse()
+# print(marks)
+
+marks.sort()
+print(marks)
+
---
+++ slices/slices.py
@@ -0,0 +1,18 @@
+# lst = ['Москва', 'Уфа', 'Тверь', 'Муром']
+#
+# print(lst[1:3])
+# print(lst[1:])
+# print(lst[:3])
+# print(lst[:])
+
+# a = lst[1:3]
+# a[0] = 'Нижний'
+# lst.append(a)
+# print(lst)
+
+marks = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(marks[0::3])
+
+
+
+
